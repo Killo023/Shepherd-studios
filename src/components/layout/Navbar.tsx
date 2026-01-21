de440e5b07c103e2d738c9009b4db25d6b9ae15e
cylehendricks23@icloud.com <cylehendricks23@icloud.com>
2026-01-21 15:19:27 +0200

    Fix mobile burger menu overlap by adjusting navbar container height

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16 md:h-20 py-1">
+        <div className="flex items-center justify-between min-h-[218px] md:min-h-[234px] lg:min-h-[250px] py-1">
           {/* Logo */}
               <Link href="/" className="flex items-center px-2 md:px-4 lg:px-6">
                 <Image
